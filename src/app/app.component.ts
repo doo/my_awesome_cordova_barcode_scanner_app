55e357e2f3f73b5a99f1a7449b5bb29ce12967f7
Rutkay Biyik <rutkay@scanbot.io>
2022-06-10 12:43:57 +0200

    Resources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import ScanbotBarcodeSDK, { ScanbotBarcodeSDKConfiguration } from 'cordova-plugin-scanbot-barcode-scanner';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
+  private barcodeSDK = ScanbotBarcodeSDK.promisify();
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,7 +19,23 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.initializeScanbotBarcodeSdk();
     });
+  }
+  
+  private async initializeScanbotBarcodeSdk(){
+    try {
+      const config: ScanbotBarcodeSDKConfiguration = {
+        licenseKey: '',
+        enableNativeLogging: true,
+        loggingEnabled: true, 
+      };
+      console.log(this.barcodeSDK);
+      await this.barcodeSDK.initializeSdk(config);
+    } catch (e) {
+      alert(JSON.stringify(e));
+    }
   }
 }
 
